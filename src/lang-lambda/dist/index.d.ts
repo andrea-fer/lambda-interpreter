282503971ddf76f073c96270500840521e60d31b
Andrea Ferenczova <andrea.ferenczova@student.tuke.sk>
2023-02-26 10:51:51 +0100

    Changed package.json, gitignore

---
+++ src/lang-lambda/dist/index.d.ts
@@ -0,0 +1,4 @@
+import { LRLanguage, LanguageSupport } from "@codemirror/language";
+declare const lambdaCalculusLanguage: LRLanguage;
+declare function LambdaLanguageSupport(): LanguageSupport;
+export { lambdaCalculusLanguage, LambdaLanguageSupport };
